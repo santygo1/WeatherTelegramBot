43a8b9f627b39caa405acda5d33cad524da54e13
santygo1 <danilkaspirin@gmail.com>
2022-08-08 22:48:48 +1000

    Simple FAQ

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,6 +1,9 @@
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.bots.AbsSender;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
@@ -25,9 +28,39 @@ public class Bot extends TelegramLongPollingBot {
         return "5524174383:AAFppMWnmJOjecQsKkMWWdm_ZWx8XlEbjOE";
     }
 
+    public void sendMsg(Message message, String answer){
+        //Preparing message
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        //Defines chat id for answer
+        sendMessage.setChatId(message.getChatId().toString());
+        //Defines message id for answer
+        sendMessage.setReplyToMessageId(message.getMessageId());
+        sendMessage.setText(answer);
+
+        //Sending message
+        try{
+            execute(sendMessage);
+        }catch (TelegramApiException exception){
+            exception.printStackTrace();
+        }
+    }
+
     //This method gets messages
     @Override
     public void onUpdateReceived(Update update) {
-
+        Message message = update.getMessage();
+        if (message != null && message.hasText()){
+            switch (message.getText()){
+                case "/help":
+                    sendMsg(message,"Чем могу помочь?");
+                    break;
+                case "/settings":
+                    sendMsg(message, "Что будем настраивать?");
+                    break;
+                default:
+            }
+        }
     }
+
 }
