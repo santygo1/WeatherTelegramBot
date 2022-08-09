2fbe39a2c92f6aed95cb14a31883e6a7a9db20af
Danil Spirin <danilkaspirin@gmail.com>
2022-08-09 12:16:18 +1000

    Parsing JSON and output result in the chat

--- src/main/java/Weather.java
+++ src/main/java/Weather.java
@@ -1,3 +1,5 @@
+import org.json.JSONArray;
+import org.json.JSONObject;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
@@ -5,17 +7,50 @@ import java.util.Scanner;
 
 public class Weather {
 
-    public static String getWeather(String message, Model model) throws IOException {
-        URL url = new URL("https://api.openweathermap.org/data/2.5/weather?q="+message+"&lang=ru&units=metric&exclude=daily&appid=90531c4609de768c32b8696946697243");
+    //Parsing the weather api url to JSON string
+    private static String parseURL(URL url) throws IOException {
+        Scanner in = new Scanner((InputStream) url.getContent());
+        StringBuilder result = new StringBuilder();
+        while (in.hasNext()) result.append(in.next());
+        return result.toString();
+    }
 
-        Scanner in = new Scanner((InputStream)url.getContent());
-        String result = "";
+    /**
+     * Parsing JSON object to printed result
+     * @param json - weather json
+     * @param model - weather model object
+     * @return printed result string for answer
+     */
+    private static String parseJSON(JSONObject json, Model model){
+        model.setName(json.getString("name"));//city
 
-        while (in.hasNext()){
-            result += in.next();
+        JSONObject main = json.getJSONObject("main");
+        model.setTemp(main.getDouble("temp"));//temperature
+        model.setHumidity(main.getDouble("humidity"));//humidity
+
+        JSONArray weatherArray = json.getJSONArray("weather");
+        for(int i = 0; i < weatherArray.length(); i++){
+            JSONObject obj = weatherArray.getJSONObject(i);
+            model.setIcon((String)obj.get("icon"));
+            model.setMain((String)obj.get("main"));
         }
 
-        return result;
+        StringBuilder result = new StringBuilder();
+        result.append("Город: ").append(model.getName()).append("\n");
+        result.append("Описание: ").append(model.getMain()).append("\n");
+        result.append("Температура: ").append(model.getTemp()).append("°C").append("\n");
+        result.append("Влажность: ").append(model.getHumidity()).append("\n");
+        result.append(" http://openweathermap.org/img/wn/").append(model.getIcon()).append(".png");
+
+        return result.toString();
     }
+    public static String getWeather(String message, Model model) throws IOException {
+        URL url = new URL("https://api.openweathermap.org/data/2.5/weather?q=" + message + "&lang=ru&units=metric&exclude=daily&appid=90531c4609de768c32b8696946697243");
+        JSONObject object = new JSONObject(parseURL(url));
+        return parseJSON(object, model);
+    }
+
+
+
 
 }
